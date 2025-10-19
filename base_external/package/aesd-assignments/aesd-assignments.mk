291bb6cf64e4fd292cc007bf7ea5c0b8d0ad3f5b
Oluwapelumi Azeez <azeezpelumi101@gmail.com>
2025-10-19 08:36:48 +0200

    Quote install paths; ensure target dirs; fix SUBDIR spacing

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -7,71 +7,47 @@
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 AESD_ASSIGNMENTS_VERSION = a4p2-fixes
-# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
-# with ssh keys and the automated build/test system.
-# Your site should start with git@github.com:
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-Oluwapelumi101.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 # Unverified 
-AESD_ASSIGNMENTS_SUBDIR = finder-app 
+AESD_ASSIGNMENTS_SUBDIR = finder-app
 # Build: ensure writer uses Buildroot toolchain (Unverified)
 AESD_ASSIGNMENTS_MAKE_OPTS = CC="$(TARGET_CC)"
-
-
-# define AESD_ASSIGNMENTS_BUILD_CMDS
-# 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
-# endef
 
 # --- Build ---
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/$(AESD_ASSIGNMENTS_SUBDIR) $(AESD_ASSIGNMENTS_MAKE_OPTS)
 endef
 
-
-# define AESD_ASSIGNMENTS_BUILD_CMDS
-# 	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/$(AESD_ASSIGNMENTS_SUBDIR) $(AESD_ASSIGNMENTS_MAKE_OPTS)
-# endef
-
-# # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
+# # --- Install into target rootfs ---
 # define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-# 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
-# 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-# 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
-
-# 	$(INSTALL) -D -m 0755 $(@D)/$(AESD_ASSIGNMENTS_SUBDIR)/finder.sh      $(TARGET_DIR)/usr/bin/finder.sh
-# 	$(INSTALL) -D -m 0755 $(@D)/$(AESD_ASSIGNMENTS_SUBDIR)/finder-test.sh $(TARGET_DIR)/usr/bin/finder-test.sh
-# 	$(INSTALL) -D -m 0755 $(@D)/$(AESD_ASSIGNMENTS_SUBDIR)/finder-test.sh $(TARGET_DIR)/usr/bin/finder-test.sh
-
-# endef
-
-
-# # Install scripts + binary; conf files to /etc/finder-app/conf
-# define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-# 	# binaries/scripts
+# 	# scripts/binaries
 # 	$(INSTALL) -D -m 0755 $(@D)/$(AESD_ASSIGNMENTS_SUBDIR)/finder.sh      $(TARGET_DIR)/usr/bin/finder.sh
 # 	$(INSTALL) -D -m 0755 $(@D)/$(AESD_ASSIGNMENTS_SUBDIR)/finder-test.sh $(TARGET_DIR)/usr/bin/finder-test.sh
 # 	$(INSTALL) -D -m 0755 $(@D)/$(AESD_ASSIGNMENTS_SUBDIR)/writer         $(TARGET_DIR)/usr/bin/writer
 
-# 	# config directory and files (0644)
+# 	# config directory and files
 # 	$(INSTALL) -d -m 0755 $(TARGET_DIR)/etc/finder-app/conf
-# 	for f in $(@D)/$(AESD_ASSIGNMENTS_SUBDIR)/conf/* ; do \
-# 		$(INSTALL) -m 0644 $$f $(TARGET_DIR)/etc/finder-app/conf/ ; \
-# 	done
+# 	$(INSTALL) -m 0644 $(@D)/$(AESD_ASSIGNMENTS_SUBDIR)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
+# 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
 # endef
 
-# --- Install into target rootfs ---
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	# scripts/binaries
-	$(INSTALL) -D -m 0755 $(@D)/$(AESD_ASSIGNMENTS_SUBDIR)/finder.sh      $(TARGET_DIR)/usr/bin/finder.sh
-	$(INSTALL) -D -m 0755 $(@D)/$(AESD_ASSIGNMENTS_SUBDIR)/finder-test.sh $(TARGET_DIR)/usr/bin/finder-test.sh
-	$(INSTALL) -D -m 0755 $(@D)/$(AESD_ASSIGNMENTS_SUBDIR)/writer         $(TARGET_DIR)/usr/bin/writer
+	$(INSTALL) -D -m 0755 "$(@D)/$(AESD_ASSIGNMENTS_SUBDIR)/finder.sh"      "$(TARGET_DIR)/usr/bin/finder.sh"
+	$(INSTALL) -D -m 0755 "$(@D)/$(AESD_ASSIGNMENTS_SUBDIR)/finder-test.sh" "$(TARGET_DIR)/usr/bin/finder-test.sh"
+	$(INSTALL) -D -m 0755 "$(@D)/$(AESD_ASSIGNMENTS_SUBDIR)/writer"         "$(TARGET_DIR)/usr/bin/writer"
 
 	# config directory and files
-	$(INSTALL) -d -m 0755 $(TARGET_DIR)/etc/finder-app/conf
-	$(INSTALL) -m 0644 $(@D)/$(AESD_ASSIGNMENTS_SUBDIR)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
+	$(INSTALL) -d -m 0755 "$(TARGET_DIR)/etc/finder-app/conf"
+	$(INSTALL) -m 0644 "$(@D)/$(AESD_ASSIGNMENTS_SUBDIR)/conf/"*            "$(TARGET_DIR)/etc/finder-app/conf/"
+
+	$(INSTALL) -d -m 0755 "$(TARGET_DIR)/bin"
+	$(INSTALL) -m 0755 "$(@D)/assignment-autotest/test/assignment4/"*       "$(TARGET_DIR)/bin"
 endef
 
 
 $(eval $(generic-package))
+             
